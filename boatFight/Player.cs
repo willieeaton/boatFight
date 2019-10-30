66e05edd1533a34f3807f0c311d167dee53f9198
Willie Eaton <philip.w.eaton@gmail.com>
2019-10-30 14:25:45 -0400

    Created README.md, started coding the Player class.

--- boatFight/Player.cs
+++ boatFight/Player.cs
@@ -3,8 +3,24 @@ namespace boatFight
 {
     public class Player
     {
-        public Player()
+        private readonly int _playerNumber;
+
+        public int PlayerNumber => _playerNumber;
+
+        public string PlayerName { get; set; }
+
+        public void CreateShip(int x, int y)
         {
+            //currently a dummy function
+            //will create a new ship
+            Console.WriteLine($"Creating ship at {x}, {y}.");
         }
+
+        public Player(int playerNumber)
+        {
+            _playerNumber = playerNumber;
+        }
+
+
     }
 }
--- boatFight/Program.cs
+++ boatFight/Program.cs
@@ -1,12 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace boatFight
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        private static void Main(string[] args)
+        { 
+            //magic number elimination station
+            const int NumberOfPlayers = 2;
+            List<Player> players = new List<Player>();
+            for (int i = 0; i < NumberOfPlayers; i++)
+            {
+                players.Add(new Player(i + 1));
+            }
+
+            for (int i = 0; i < NumberOfPlayers; i++)
+            {
+                Console.Write($"Player {players[i].PlayerNumber}, please enter your name. ");
+                string playerName = Console.ReadLine();
+                players[i].PlayerName = playerName;
+            }
+
+            for (int i = 0; i < NumberOfPlayers; i++)
+            {
+                Console.Clear();
+                Console.Write($"{players[i].PlayerName}, please enter your X coordinate, from 1 to DUMMY. ");
+                int coordinateX = int.Parse(Console.ReadLine());
+                Console.Write($"{players[i].PlayerName}, please enter your Y coordinate, from 1 to DUMMY. ");
+                int coordinateY = int.Parse(Console.ReadLine());
+
+                players[i].CreateShip(coordinateX, coordinateY);
+            }
         }
     }
 }
